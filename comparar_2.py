eeb0951ce095d1bc396be78cd450ba6f2ea01469
DiegoVivero <d.viverovalenzuela@uandresbello.edu>
2024-08-17 01:19:29 +0000

    Comparar completo

---
+++ comparar_2.py
@@ -0,0 +1,10 @@
+x = input("¿Cúal es el valor de x?")
+y = input("¿Cúal es el valor de y")
+
+if x < y:
+    print("x es menor que y")
+if x > y:
+    print("x es mayor que y")
+if x==y:
+    print("x es igual a y")
+    
