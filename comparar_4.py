4d97bfb2947dd28968d73d064f3515c4d4e39feb
DiegoVivero <d.viverovalenzuela@uandresbello.edu>
2024-08-17 01:30:03 +0000

    Comparación Mejorada Else

---
+++ comparar_4.py
@@ -0,0 +1,9 @@
+x = input("¿Cúal es el valor de x?")
+y = input("¿Cúal es el valor de y")
+
+if x < y:
+    print("x es menor que y")
+elif x > y:
+    print("x es mayor que y")
+else:
+    print("x es igual a y")
