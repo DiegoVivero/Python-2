cf1908faec5d94ec2c6708681978fe69fd612a67
DiegoVivero <d.viverovalenzuela@uandresbello.edu>
2024-08-19 23:59:57 +0000

    Cambio 2

---
+++ equipo_1.py
@@ -0,0 +1,13 @@
+jugador = input("Ingresa un jugador de fútbol: ")
+
+if jugador == "Vidal": 
+    print("Colo Colo")
+elif jugador == "Matador": 
+    print("La U")
+elif jugador == "Palacios": 
+    print("Colo Colo")
+elif jugador == "Falcón":
+    print("Colo Colo")
+else: 
+    print("Quién")
+
