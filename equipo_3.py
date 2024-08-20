e85f50e411c750103e5c5677e6631e862f531f02
DiegoVivero <d.viverovalenzuela@uandresbello.edu>
2024-08-20 00:56:22 +0000

    Equipo 3

--- equipo_3.py
+++ equipo_3.py
@@ -1,3 +1,14 @@
 jugador = input("Ingresa un jugador de fútbol: ")
 
-match jugador
+match jugador: 
+    case "Vidal": 
+        print("Colo Colo")
+    case "Matador":
+        print("La U")
+    case "Palacios":
+        print("Colo Colo")
+    case "Falcón":
+        print("Colo Colo")
+    case _: 
+        print("Quién")
+
