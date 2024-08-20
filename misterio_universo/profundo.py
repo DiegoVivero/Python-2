b3d830d5a8007a46f01001fcbdee088636b24b84
DiegoVivero <d.viverovalenzuela@uandresbello.edu>
2024-08-20 01:44:40 +0000

    Universo.

---
+++ misterio_universo/profundo.py
@@ -0,0 +1,9 @@
+respuesta_sentido_universo = input("¿Cuál es el misterio del universo? ")
+
+respuesta_sentido_universo = respuesta_sentido_universo.lower().strip().split()
+respuesta_sentido_universo = ' '.join(respuesta_sentido_universo)
+if respuesta_sentido_universo == "42" or respuesta_sentido_universo == "cuarenta y dos" : 
+    print("Si")
+else: 
+    print("No")
+
