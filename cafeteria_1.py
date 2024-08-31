efd3576a357bac7bf0d1d8ca2be198ead8b7c2cc
DiegoVivero <d.viverovalenzuela@uandresbello.edu>
2024-08-31 01:02:42 +0000

    o

---
+++ cafeteria_1.py
@@ -0,0 +1,11 @@
+cafe = int(input("Cúanto cafe quiere: "))
+te = int(input("Cúanto te quiere: "))
+jugo natural = int(input("Cúanto jugo natural quiere: "))
+
+#Precios
+p_cafe = 1500
+p_te = 1000 
+p_jugo natural = 2000
+Precio_total = cafe * p_cafe + te * p_te + jugo_natural * p_jugo natural
+
+print(f"El precio total de su pedido es: {Precio_total}")
