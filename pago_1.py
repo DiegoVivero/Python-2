2e7ece3e5b6a554651b2ddafc652a9a8fc9426b3
DiegoVivero <d.viverovalenzuela@uandresbello.edu>
2024-09-02 23:36:21 +0000

    Pago

---
+++ pago_1.py
@@ -0,0 +1,10 @@
+#Solicitar ingreso al usuario
+precio = int(input("Ingrese el precio: "))
+monto_pago = int(input("Ingrese el pago: "))
+
+# Realizar cálculos
+vuelto = monto_pago - precio
+
+# Mostrar el resultado
+print(f"El vuelto es {vuelto}")
+
