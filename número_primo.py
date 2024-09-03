e6c65baf9ae683836af54a7391f38a8e33071181
DiegoVivero <d.viverovalenzuela@uandresbello.edu>
2024-09-03 01:11:20 +0000

    Number primo

--- número_primo.py
+++ número_primo.py
@@ -3,5 +3,9 @@ n = int(input("Ingresa número entero: "))
 for i in range(2, n): 
     if n % i == 0: 
         print(f"{n} no es número primo")
-    else: 
+        break
+    else:
         print(f"{n} es número primo")
+        break
+
+
