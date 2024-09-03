216ec89cc432dafa7395307b871b3d13d9052eff
DiegoVivero <d.viverovalenzuela@uandresbello.edu>
2024-09-03 00:02:46 +0000

    pago 3

--- pago_1.py
+++ pago_1.py
@@ -6,11 +6,13 @@ monto_pago = int(input("Ingrese el pago: "))
 vuelto = monto_pago - precio
 
 # Calcular cuantos billetes de a $1.000 se requieren
-
 billetes_1000 = int(vuelto / 1000) 
+vuelto = vuelto - (billetes_1000 * 1000) #Vuelto % 1000
 
-print(billetes_1000)
+# Calcular cuantos billetes de a $500 se requieren 
+monedas_500 = int(vuelto / 500)
 
 # Mostrar el resultado
-print(f"El vuelto es {vuelto}")
+print(f" {billetes_1000} billetes de $1.000")
+print(f" {monedas_500} monedas de $500")
 
