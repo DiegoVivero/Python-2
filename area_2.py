003511b65be37316b9fbeee78fb442e596b11a39
DiegoVivero <d.viverovalenzuela@uandresbello.edu>
2024-09-03 00:39:39 +0000

    area 2

--- area_2.py
+++ area_2.py
@@ -4,7 +4,9 @@ lado = float(input("Ingrese el lado del cuadrado: "))
 area = lado ** 2
 # Calcular el area del triangulo equilátero
 area_triangulo = ((3 ** (0.5))/4) * (lado ** 2)
+# Calcular el área de un pentágono regular
+area_pentagono = (lado ** 2 * (25 + 10 * 5 ** (0.5)) ** (0.5))/4
 # Mostrar resultado
 print(f"El área del cuadrado es: {area:.2f}")
 print(f"El área del triangulo es: {area_triangulo:.2f}")
-
+print(f"El área del pentágono es: {area_pentagono:.2f}")
