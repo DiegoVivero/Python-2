fa9f7a21312b62181d3131414a8f48e1fd3ec947
DiegoVivero <d.viverovalenzuela@uandresbello.edu>
2024-09-03 01:32:41 +0000

    ok

--- es_primo.py
+++ es_primo.py
@@ -2,5 +2,10 @@ def es_primo(n):
     for i in range(2, n):
         if n % i == 0: 
             return False
-        else:
-            return True
+        return True
+
+es_primo = es_primo(7)
+if es_primo:
+    print("El número es primo")
+else: 
+    print("El número no es primo")
