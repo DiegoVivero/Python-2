fa5585ad1095c538f07f16abb1442f104c74da2d
DiegoVivero <d.viverovalenzuela@uandresbello.edu>
2024-09-10 00:58:48 +0000

    Multiplo

---
+++ 1_par_impar.py
@@ -0,0 +1,9 @@
+# Escribir si un número es par o impar usar n % 2
+# Solicitar ingreso de un número 
+
+n = int(input("Ingrese un número: "))
+
+if n % 2 == 0: 
+    print("es Par")
+else: 
+    print("es Impar")
---
+++ 2_multiplo.py
@@ -0,0 +1,11 @@
+# Verificar si un número es multiplo de otro 
+a = int(input("Ingrese el primer número: "))
+b = int(input("Ingrese el segundo número: "))
+
+if a % b == 0: 
+    print(f"{a} es multiplo de {b}")
+else: 
+    print(f"{a} no es multiplo de {b}")
+
+    
+    
