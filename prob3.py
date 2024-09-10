efe4965aa96eee732492882adc6436d3923f1273
DiegoVivero <d.viverovalenzuela@uandresbello.edu>
2024-09-10 00:28:56 +0000

    prueba ej3

--- prob3.py
+++ prob3.py
@@ -2,14 +2,14 @@ n = int(input("Ingresa un número entero positivo: "))
 
 es_primo = True
 
-for i in range(2, n): 
-    resto = n % i 
+for i in range(2,n): 
+    resto = n % i
     if resto == 0:
         es_primo = False 
         break
 
-    if es_primo: 
-        print(f"{n} es primo")
-    else:
-        print(f"{n} no es primo")
+if es_primo: 
+    print(f"El {n} es primo")
+else:
+    print(f"El {n} no es primo")
 
