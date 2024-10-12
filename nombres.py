8eb72845412af3bd907b5c5be130e2d41af48c62
DiegoVivero <d.viverovalenzuela@uandresbello.edu>
2024-10-12 00:32:07 +0000

    Nombres_1

--- nombres.py
+++ nombres.py
@@ -1,2 +1,8 @@
-import sys 
-print("hola, mi nombre es", sys.argv[1])
+import sys
+if len(sys.argv) < 2:
+    print("Muy pocos argumentos")
+elif len(sys.argv) > 2:
+    print("Demasiados argumentos")
+else:
+    print("Hola, mi nombre es", sys.argv[1])
+    
