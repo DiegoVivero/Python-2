e19297c39a4f3565dab58bbc576c5588499cca58
DiegoVivero <d.viverovalenzuela@uandresbello.edu>
2024-10-22 00:14:57 +0000

    Jugador

---
+++ jugador1.py
@@ -0,0 +1,16 @@
+# Jugador de fútbol con variables
+class Jugador:
+    def __init__
+    ...
+
+def main(): 
+    jugador = get_jugador()
+    print(f"{jugador.nombre} es de {jugador.equipo}")
+
+def get_jugador(): 
+    jugador = jugador()
+    jugador.nombre = input("Nombre: ")
+    jugador.equipo = input("Equipo: ")
+    return jugador
+
+if __name__ == "__main__":
