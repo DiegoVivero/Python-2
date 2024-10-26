52eef8c7976c1732c8fb44452dcd796b6fd363b1
DiegoVivero <d.viverovalenzuela@uandresbello.edu>
2024-10-26 00:48:51 +0000

    Perro2

--- perro1.py
+++ perro1.py
@@ -1,8 +1,14 @@
 class Perro: 
+
+    def __init__(self, nombre):
+        self.nombre = nombre
+
     def ladrar(self):
         print("guau")
 
-p = Perro()
+p = Perro("Manchita")
+print(p.nombre)
 p.ladrar()
-p2 = Perro()
+p2 = Perro("Wally")
 p2.ladrar()
+print(p2.nombre)
